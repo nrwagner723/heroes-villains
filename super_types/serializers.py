44fe7c9ddd0a9c696e72a0c506b967b4188118e6
Natalie Wagner <nrwagner723@gmail.com>
2022-06-25 17:49:22 -0500

    updated urls + created first views function

--- super_types/serializers.py
+++ super_types/serializers.py
@@ -4,4 +4,4 @@ from .models import SuperTypes
 class SuperTypesSerializer(serializers.ModelSerializer):
     class Meta:
         model = SuperTypes
-        fields = ['id', 'types']
+        fields = ['id', 'type']
