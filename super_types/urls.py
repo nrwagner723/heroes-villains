ef92d875c36eb5b5e02cee3d0ace5d0e1163f180
Natalie Wagner <nrwagner723@gmail.com>
2022-06-26 15:30:15 -0500

    started working on post functionality for supers model

--- super_types/urls.py
+++ super_types/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path
-from . import views
+# from django.urls import path
+# from . import views
 
-urlpatterns = [
-    path('', views.cars_list),
-    path('<int:pk>/', views.car_detail),
-]
+# urlpatterns = [
+#     path('', views._list),
+#     path('<int:pk>/', views._detail),
+# ]
